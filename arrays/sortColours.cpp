9da9f0abe8e969bec125aa23efbd339d676cda53
Riddhi Sharma <54465486+riddhisharma2000@users.noreply.github.com>
2020-08-06 12:42:25 +0530

    Create sortColours.cpp

---
+++ arrays/sortColours.cpp
@@ -0,0 +1,37 @@
+
+// C++ code for Sort colours from leetcode.
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int low = 0,mid = 0;
+        int high = nums.size() - 1;
+        
+        while(mid <= high)
+        {
+            if(nums[mid] == 0)
+            {
+                swap(nums[low++], nums[mid++]);
+                continue;
+            }
+            
+            if(nums[mid] == 1)
+            {
+                mid++;
+                continue;
+            }
+            
+            if(nums[mid] == 2)
+            {
+               swap(nums[mid], nums[high--] );
+                continue;
+            }
+        }
+        
+        for( auto i : nums)
+        {
+            cout<<i<<",";
+        }
+    }
+};
